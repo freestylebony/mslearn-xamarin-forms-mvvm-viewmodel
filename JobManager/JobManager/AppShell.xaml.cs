c9a413a82dd1a0ffd37109fb12767a2461b6ac84
hafees <hafees.falana@gmail.com>
2022-03-09 19:01:31 -0500

    (00:21:15) A clean startup project achieved. There was a cleanup of a whole project solution to start the Job Manager app.

---
+++ JobManager/JobManager/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace JobManager
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+          
+        }
+
+       
+    }
+}
